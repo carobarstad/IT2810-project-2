3cf84836b6268763d924cd02c35fac405cccd1be
Caroline Barstad <caroba@stud.ntnu.no>
2020-09-11 16:11:23 +0200

    Lagt til toggle for dark mode
    Kun for body og text så langt

--- kunstutstilling/src/App.tsx
+++ kunstutstilling/src/App.tsx
@@ -1,25 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { ThemeProvider } from 'styled-components';
+import { lightTheme, darkTheme } from './theme';
+import { GlobalStyles } from './global';
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
+
+  const [theme, setTheme] = useState('light');
+
+  const toggleTheme = () => {
+    if (theme === 'light') {
+      setTheme('dark');
+    } else {
+      setTheme('light');
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+      <>
+        <GlobalStyles />
+        <button onClick={toggleTheme}>Toggle theme</button>
+        <h1>It's a toggle theme!:)</h1>
+        <footer>
+        </footer>
+      </>
+    </ThemeProvider>
   );
 }
 
---
+++ kunstutstilling/src/components/Toggle.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { func, string } from 'prop-types';
+import styled from 'styled-components';
+
+function Toggle() {
+    <h1>test</h1>
+}
