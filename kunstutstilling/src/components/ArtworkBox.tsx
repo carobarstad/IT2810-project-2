6e2117235df68419997d3d07885da362f83c6258
Simen <simesal@stud.ntnu.no>
2020-09-21 16:55:10 +0200

    Added session-support for welcome-page

--- kunstutstilling/src/components/ArtworkBox.tsx
+++ kunstutstilling/src/components/ArtworkBox.tsx
@@ -1,14 +1,34 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
+import Heart from '../svg/heart.svg'
+import Filled from '../svg/heart_filled.svg'
 
-export default class ArtworkBox extends Component {
-    render() {
-        return (
-            <div className="ArtworkBox">
-                <div className="Artwork">
-                    Insert artwork here
-                </div>
-                <p>Her skal det stå et dikt</p>
-            </div>
-        )
+export default function ArtworkBox() {
+
+    const [liked, setLiked] = useState(false)
+    const [image, setImage] = useState(Heart)
+
+    function handleClick(){
+        if(liked){
+            setLiked(false)
+            setImage(Heart)
+        } else {
+            setLiked(true)
+            setImage(Filled)
+        }
     }
+
+    
+
+    return (
+        <div className="ArtworkBox">
+            <div className="Artwork">
+                Insert artwork here
+            </div>
+            <button className="PortraitToggleButton" onClick={()=>handleClick()}>
+                <img className="LikeButton" src={image} alt="like" width="30" height="30"></img>
+            </button>
+            <p>Her skal det stå et dikt</p>
+        </div>
+    )
+    
 }
