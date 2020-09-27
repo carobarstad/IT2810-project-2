c854df7371d5d0a73b477cd256131d31c33c1d1c
Simen <simesal@stud.ntnu.no>
2020-09-27 15:43:16 +0200

    Fixed portraittogglebutton to change color on theme change

---
+++ kunstutstilling/src/svg/PortraitNight.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function Portrait() {
+    return (
+        <div className="Portrait">
+            <svg className="icons" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50">
+                <g fill="#f7f7f7">
+                    <circle cx="25" cy="65" r="30"/>
+                    <circle cx="25" cy="25" r="15"/>
+                </g>
+            </svg>
+        </div>
+    );
+}
+
+
+
