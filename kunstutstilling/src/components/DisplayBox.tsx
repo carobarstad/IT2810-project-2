66c0fd48961c3120618f2a1a3e3cd7c015f677fb
Hanna Torjusen <hanna.torjusen@gmail.co>
2020-09-28 17:35:58 +0200

    removed for loop for artworkboxes

--- kunstutstilling/src/components/DisplayBox.tsx
+++ kunstutstilling/src/components/DisplayBox.tsx
@@ -58,12 +58,106 @@ const content = <div className="DisplayBox">
         </div>
         <PortraitToggleButton />
       </div>
-
       {/* Render if poemDB hasn't loaded */}
-      {props.loading && contentLoading}
-    
+      {props.loading && (
+        <div className="DisplayBox">
+          <ArtworkBox
+            poetry={props.poetry[0]}
+            imgNr={1}
+            identifier="1"
+            liked={localStorage.getItem("artwork1")}
+            image=""
+          />
+          <ArtworkBox
+            poetry={props.poetry[0]}
+            imgNr={2}
+            identifier="2"
+            liked={localStorage.getItem("artwork2")}
+            image=""
+          />
+          <ArtworkBox
+            poetry={props.poetry[0]}
+            imgNr={3}
+            identifier="3"
+            liked={localStorage.getItem("artwork3")}
+            image=""
+          />
+          <ArtworkBox
+            poetry={props.poetry[0]}
+            imgNr={4}
+            identifier="4"
+            liked={localStorage.getItem("artwork4")}
+            image=""
+          />
+          <ArtworkBox
+            poetry={props.poetry[0]}
+            imgNr={5}
+            identifier="5"
+            liked={localStorage.getItem("artwork5")}
+            image=""
+          />
+          <ArtworkBox
+            poetry={props.poetry[0]}
+            imgNr={6}
+            identifier="6"
+            liked={localStorage.getItem("artwork6")}
+            image=""
+          />
+          <div className='NoFavoritesMessage'>
+            <h2>You have no favorites</h2>
+          </div>
+        </div>
+      )}
       {/* Render if poetryDB had loaded */}
-      {!props.loading && content}
+      {!props.loading && (
+        <div className="DisplayBox">
+          <ArtworkBox
+            poetry={props.poetry[0]}
+            imgNr={1}
+            identifier="1"
+            liked={localStorage.getItem("artwork1")}
+            image=""
+          />
+          <ArtworkBox
+            poetry={props.poetry[1]}
+            imgNr={2}
+            identifier="2"
+            liked={localStorage.getItem("artwork2")}
+            image=""
+          />
+          <ArtworkBox
+            poetry={props.poetry[2]}
+            imgNr={3}
+            identifier="3"
+            liked={localStorage.getItem("artwork3")}
+            image=""
+          />
+          <ArtworkBox
+            poetry={props.poetry[3]}
+            imgNr={4}
+            identifier="4"
+            liked={localStorage.getItem("artwork4")}
+            image=""
+          />
+          <ArtworkBox
+            poetry={props.poetry[4]}
+            imgNr={5}
+            identifier="5"
+            liked={localStorage.getItem("artwork5")}
+            image=""
+          />
+          <ArtworkBox
+            poetry={props.poetry[5]}
+            imgNr={6}
+            identifier="6"
+            liked={localStorage.getItem("artwork6")}
+            image=""
+          />
+          <div className='NoFavoritesMessage'>
+            <h2>You have no favorites</h2>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
